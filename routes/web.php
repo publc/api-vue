8a2f5c96dfd01a9f968615a53694704cdd22842d
Diego Londono <dl@deveplon.com>
2018-07-22 18:42:53 -0300

    Delete some files don't use it

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
 $app->get('/', 'Home\HomeController@index');
-$app->get('/seminarios', 'Home\SeminarsController@index');
-$app->get('/productos', 'Home\ProductsController@index');
-$app->get('/nosotros', 'Home\AboutController@index');
-$app->get('/contacto', 'Home\ContactController@index');
 
 $app->get('/admin', 'Admin\AdminController@index');
